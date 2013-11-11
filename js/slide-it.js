4e3c7db85cda95c0dc4bf4db3c3a06b85f235cba
chbymnky <chbymnky@gmail.com>
2013-11-11 17:18:36 -0600

    JS: started carousel
    some minor CSS adjustments but mostly JS

---
+++ js/slide-it.js
@@ -0,0 +1,57 @@
+/**
+ * Created by chbymnky on 11/10/13.
+ */
+// create slideshow functionality for nat
+slideshow = function (slideCont) {
+	this.slidecont = slideCont;
+	this.slides = slideCont.children();
+	this.imgs = slideCont.find('img');
+	this.sWidth = this.slides.width();
+	this.sHeight = this.slides.height();
+
+	// constrict the slideshow container for visual purposes
+	this.slidecont.css('width', this.sWidth+'px')
+								.css('height', this.sHeight+'px')
+								.css('overflow', 'hidden');
+}
+
+slideshow.prototype.createCarousel = function () {
+	this.thmbCont = $('<div class="slide-thumb-cont"></div>').insertAfter(this.slidecont);
+
+	for (var i = 0; i < this.slides.length; i++)
+	{
+		this.thmbCont.append('<div class="slide-thumb-item left" data-num="'+[i]+'">'+
+														'<img src="'+this.imgs[i]['src']+'">'+
+													'</dvi>');
+	}
+}
+
+slideshow.prototype.gotoSlide = function () {
+
+}
+
+slideshow.prototype.startSlides = function () {
+
+}
+
+var cycle = new slideshow($('.slideshow'));
+cycle.createCarousel();
+
+/*$(function (){
+	$('.slideshow').each(function () {
+		var $slideCont = $(this),
+				$slide = $slideCont.children();
+		var imgs = [],
+				swidth = $slide.width(),
+				sheight = $slide.height();
+
+		for (var i = 0; i < $slide.length(); i++)
+		{
+			imgs.push($slide[i].find('img').attr('src'));
+		}
+
+	});
+});*/
+
+
+
