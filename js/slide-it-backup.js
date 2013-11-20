274f6615f2c34a17f98fa49a77873291c3bccbba
chbymnky <chbymnky@gmail.com>
2013-11-19 21:08:45 -0600

    JS and style clean up

--- js/slide-it-backup.js
+++ js/slide-it-backup.js
@@ -1,25 +1,22 @@
 /**
  * Created by chbymnky on 11/10/13.
  */
-
 // create slideshow functionality for nat
-function slideshow($slideContIn, intervalIn) {
-	this.$slideCont = $slideContIn;
-	this.$slides = this.$slideCont.children();
-	this.$imgs = this.$slideCont.find('img');
-	this.$thmbCont = $('<div class="slide-thumb-cont"></div>');
+var slideshow = function (slideCont, interval) {
+	this.$slidecont = slideCont;
+	this.$slides = slideCont.children();
+	this.imgs = slideCont.find('img');
 	this.sWidth = this.$slides.width();
 	this.sHeight = this.$slides.height();
-	this.interval = intervalIn;
-	this.intervalID = null;
-
-	this.initiate();
+	this.interval = interval;
+	this.$thmbCont = $('<div class="slide-thumb-cont"></div>');
+	this.intervalID = 'Null';
 }
 
 slideshow.prototype = {
-	initiate: function() {
+	init: function () {
 		// constrict the slideshow container for visual congruency
-		this.$slideCont.width(this.sWidth)
+		this.$slidecont.width(this.sWidth)
 				.height(this.sHeight)
 				.css('postion', 'relative');
 		// position all slides for transition effect
@@ -28,20 +25,24 @@ slideshow.prototype = {
 		// mark first slide as the active slide and hide the rest
 		$(this.$slides[0]).addClass('is-active').show().siblings().hide();
 
+		this.$thmbCont.insertAfter(this.$slidecont);
 		// create individual thumbnails for the carousel
-		this.$thmbCont.insertAfter(this.$slideCont);
 		this.createThmbs();
-		this.resume();
+
+		//var transslide = this.transSlide;
+
+		slideshow.intervalID = setInterval(this.transSlide, this.interval);
 	},
-	transSlide: function($inSlidesCont, $inThumbsCont) {
-		var $slides = $inSlidesCont.children(),
-				$thumbs = $inThumbsCont.children(),
+	transSlide: function () {
+		var $slides = this.$slides,
+				$thumbs = this.$thmbCont.children(),
 				activeIndex = $slides.filter('.is-active').index(),
 				activeThmb = $thumbs.filter('.is-active').index(),
-				maxSlides = $slides.length;
+				maxSlides = $slides.length,
+				transSlideFN = this.transSlide;
 
-		// change active slide index the next slide
-		if (activeIndex !== activeThmb) {
+		// change to the index of the next slide
+		if (activeIndex != activeThmb) {
 			activeIndex = activeThmb;
 		}
 		else if (activeIndex < (maxSlides - 1)) {
@@ -56,22 +57,14 @@ slideshow.prototype = {
 		$slides.fadeOut().filter('.is-active').fadeIn();
 		$thumbs.removeClass('is-active').eq(activeIndex).addClass('is-active');
 
-		// halting slideshow functionality
-		/*$slides.bind('mouseover', { slct: this }, function () {
-			console.log(event.data.slct);
-			//event.data.slct.pause;
-		});
-		$slides.bind('mouseout', { slct: this }, function () {
-			console.log(event.data.slct);
-			//event.data.slct.resume;
-		});*/
 	},
 	createThmbs: function () {
 		// place HTML markup for each thumbnail in the carousel
-		for (var i = 0; i < this.$slides.length; i++) {
-			this.$thmbCont.append('<div class="slide-thumb-item left" data-num="' + [i] + '">' +
-					'<img src="' + this.$imgs[i].src + '">' +
-					'</dvi>');
+		for (var i = 0; i < this.$slides.length; i++)
+		{
+			this.$thmbCont.append('<div class="slide-thumb-item left" data-num="'+[i]+'">'+
+															'<img src="'+this.imgs[i].src+'">'+
+														'</dvi>');
 		}
 
 		// set active thumbnail and bind click event to all
@@ -80,28 +73,29 @@ slideshow.prototype = {
 				.addClass('is-active');
 	},
 	transThmb: function (thmbIndex) {
-		var $thmbs = this.$thmbCont.children();
-		$thmbs.removeClass('is-active').eq(thmbIndex).addClass('is-active');
+		var thmbs = this.$thmbCont.children();
+
+		thmbs.removeClass('is-active').eq(thmbIndex).addClass('is-active');
 		this.pause();
-		this.transSlide(this.$slideCont, this.$thmbCont);
+		this.transSlide();
 		this.resume();
 	},
 	pause: function () {
 		clearInterval(this.intervalID);
 	},
 	resume: function () {
-		var $slideCont = this.$slideCont,
-				$thmbCont = this.$thmbCont,
-				transSlide = this.transSlide;
-		this.intervalID = setInterval(function () {
-			transSlide($slideCont, $thmbCont);
-		}, this.interval);
+		this.intervalID = setInterval(this.transSlide, this.interval);
 	}
-};
+}
 
 var $slideshowSlct = $('.slideshow'),
-		slideIt = new slideshow($slideshowSlct, 2000);
+		slideIt = new slideshow($slideshowSlct, 6000);
 
+slideIt.init();
+
+// halting slideshow functionality
+$slideshowSlct.bind('mouseover', slideIt.pause);
+$slideshowSlct.bind('mouseout', slideIt.resume);
 
 // change active class for thumbnails
 var thmbs = $('.slide-thumb-item');
@@ -112,3 +106,7 @@ thmbs.bind('click', function () {
 	//elm.addClass('is-active').siblings().removeClass('is-active');
 	slideIt.transThmb(elmIndex);
 });
+
+
+
+
